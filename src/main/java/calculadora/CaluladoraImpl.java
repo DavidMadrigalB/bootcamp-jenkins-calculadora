fcece045032c6ce3d59c04550d14c133995f30a9
David Madrigal Buendía <davidmadrigalbuendia@gmail.com>
2024-10-23 17:32:45 -0600

    Update CaluladoraImpl.java
    Fix lógico en calculadora

--- src/main/java/calculadora/CaluladoraImpl.java
+++ src/main/java/calculadora/CaluladoraImpl.java
@@ -4,7 +4,7 @@ public class CaluladoraImpl implements Calculadora {
 	
 	@Override
 	public int calcular(int i1, char op, int i2) {
-		switch(op)
+		switch(op) {
 			case '+':
 				return i1 + i2;
 				break;
@@ -19,6 +19,7 @@ public class CaluladoraImpl implements Calculadora {
 				break;
 			default:
 				return -1;
+		}
 		/*
 		if (op == '+')
 			return i1 + i2;

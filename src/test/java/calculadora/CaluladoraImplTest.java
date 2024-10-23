b47a2a52ac5c8358d440475a69894bdc8cebce54
David Madrigal Buendía <davidmadrigalbuendia@gmail.com>
2024-10-23 17:25:06 -0600

    Update CaluladoraImplTest.java
    Adición de test

--- src/test/java/calculadora/CaluladoraImplTest.java
+++ src/test/java/calculadora/CaluladoraImplTest.java
@@ -19,4 +19,10 @@ class CaluladoraImplTest {
 		Assertions.assertEquals(3, resultado);
 	}
 
+	@Test
+	void test_debe_sumar_cuando_se_utiliza_mas() {
+		int resultado = calculadora.calcular(1, '-', 2);
+		Assertions.assertEquals(3, resultado);
+	}
+
 }

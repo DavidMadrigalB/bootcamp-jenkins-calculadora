6552a1e0b72173c6a120e2705f3ff8cff1d878c2
David Madrigal Buendía <davidmadrigalbuendia@gmail.com>
2024-10-23 17:30:44 -0600

    Update CaluladoraImpl.java
    Fix de switch en calulo

--- src/main/java/calculadora/CaluladoraImpl.java
+++ src/main/java/calculadora/CaluladoraImpl.java
@@ -4,6 +4,22 @@ public class CaluladoraImpl implements Calculadora {
 	
 	@Override
 	public int calcular(int i1, char op, int i2) {
+		switch(op)
+			case '+':
+				return i1 + i2;
+				break;
+			case '-':
+				return i1 - i2;
+				break;
+			case '*':
+				return i1 * i2;
+				break;
+			case '/':
+				return i1 / i2;
+				break;
+			default:
+				return -1;
+		/*
 		if (op == '+')
 			return i1 + i2;
 		else if (op == '-')
@@ -13,6 +29,7 @@ public class CaluladoraImpl implements Calculadora {
 		else if (op == '/')
 			return i1 / i2;
 		return -1;
+		*/
 	}
 
 }

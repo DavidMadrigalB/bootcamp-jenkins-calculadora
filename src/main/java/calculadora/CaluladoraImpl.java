1e8972f16dd98b913b570a28433ab9c4ab323eb5
David Madrigal Buendía <davidmadrigalbuendia@gmail.com>
2024-10-23 17:24:30 -0600

    Update CaluladoraImpl.java
    Fix de feature en el cálculo

--- src/main/java/calculadora/CaluladoraImpl.java
+++ src/main/java/calculadora/CaluladoraImpl.java
@@ -4,13 +4,13 @@ public class CaluladoraImpl implements Calculadora {
 	
 	@Override
 	public int calcular(int i1, char op, int i2) {
-		if (op = '+')
+		if (op == '+')
 			return i1 + i2;
-		else if (op = '-')
+		else if (op == '-')
 			return i1 + i2;
-		else if (op = '*')
+		else if (op == '*')
 			return i1 * i2;
-		else if (op = '/')
+		else if (op == '/')
 			return i1 / i2;
 	}
 

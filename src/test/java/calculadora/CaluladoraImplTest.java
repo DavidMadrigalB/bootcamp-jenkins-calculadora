dd6dbeaff5683c70ac3e51a9b5fac22caa2aa799
David Madrigal Buendía <davidmadrigalbuendia@gmail.com>
2024-10-23 17:25:32 -0600

    Update CaluladoraImplTest.java
    Fix en test

--- src/test/java/calculadora/CaluladoraImplTest.java
+++ src/test/java/calculadora/CaluladoraImplTest.java
@@ -20,7 +20,7 @@ class CaluladoraImplTest {
 	}
 
 	@Test
-	void test_debe_sumar_cuando_se_utiliza_mas() {
+	void test_debe_restar_cuando_se_utiliza_menos() {
 		int resultado = calculadora.calcular(1, '-', 2);
 		Assertions.assertEquals(3, resultado);
 	}

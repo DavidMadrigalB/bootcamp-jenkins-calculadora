8059dd2dbe67f5519b54f0cfba7f957f3d06e100
David Madrigal Buendía <davidmadrigalbuendia@gmail.com>
2024-10-31 16:29:33 -0600

    Update CaluladoraImplTest.java

--- src/test/java/calculadora/CaluladoraImplTest.java
+++ src/test/java/calculadora/CaluladoraImplTest.java
@@ -28,7 +28,7 @@ class CaluladoraImplTest {
 	@Test
 	void test_debe_multiplizar_cuando_se_utiliza_por() {
 		int resultado = calculadora.calcular(3, '*', 2);
-		Assertions.assertEquals(0, resultado);
+		Assertions.assertEquals(6, resultado);
 	}
 
 }
